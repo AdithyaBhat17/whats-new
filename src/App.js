3d594949389a53b8304bf95b252627d6fc84b653
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-01-06 22:09:54 +0530

    ✨ concat json data to state data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import axios from 'axios'
+
+// color palette = // 5A7BB5 • CCDCF2 • F4F6FC • 70DEF1⠀
 
 export default class App extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            data: []
+        }
+    }    
+    componentDidMount(){
+        axios.get('https://newsapi.org/v2/everything?q=bitcoin&apiKey=apikeygoeshere')
+        .then((resp) => {
+            let data = this.state.data.concat(resp.data.articles)
+            this.setState({data})
+        })
+    }
     render(){
         return(
-            <div>hi</div>
+            <div>
+                Will work on this tmrw
+            </div>
         )
     }
 }
