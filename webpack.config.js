92238492f4fd7d5bc8d22aca01b79a4becc950ca
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-01-06 22:12:52 +0530

    🔧 webpack configuration
    TODO : Add env support

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,23 @@
 const path = require('path')
-const hwp = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    entry: path.join(__dirname, '/src/index.js'),
+    entry: './src/index.js',
     output: {
-        filename: 'build.js',
-        path: path.join(__dirname, './dist')
+        path: path.join(__dirname, '/dist'),
+        filename: 'build.js'
     },
     module: {
         rules: [{
             test: /\.js$/,
             exclude: /node_modules/,
-            loader: 'babel-loader'
+            use: ["babel-loader"]
         }]
     },
     plugins: [
-        new hwp({
-            template: path.join(__dirname, '/src/index.html')
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
         })
     ]
 }
