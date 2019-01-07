026258c5cdca028e535a8f8a5610981fc21e3ea9
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-01-07 15:28:56 +0530

    🔧 trying out dotenv

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,15 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const dotenv = require('dotenv')
+
+const env = dotenv.config().parsed;
+  
+  // reduce it to a nice object, the same as before
+const envKeys = Object.keys(env).reduce((prev, next) => {
+prev[`process.env.${next}`] = JSON.stringify(env[next]);
+return prev;
+}, {});
 
 module.exports = {
     entry: './src/index.js',
@@ -18,6 +28,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './src/index.html',
             filename: './index.html'
-        })
+        }),
+        new webpack.DefinePlugin(envKeys)
     ]
 }
