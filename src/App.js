e4e07abcc93f30eab890a8a82cf31780789ed0da
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-01-07 15:27:41 +0530

    💩 renders 20 articles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react'
 import axios from 'axios'
-
-// color palette = // 5A7BB5 • CCDCF2 • F4F6FC • 70DEF1⠀
+import { Nav, Thumbnail, Img } from './style'
+import Navbar from './Navbar'
 
 export default class App extends React.Component{
     constructor(props){
         super(props)
         this.state = {
-            data: []
+            data: [],
+            loading: false
         }
     }    
     componentDidMount(){
-        axios.get('https://newsapi.org/v2/everything?q=bitcoin&apiKey=apikeygoeshere')
+        console.log(process.env.APIKEY)
+        axios.get(`https://newsapi.org/v2/everything?q=everything&apiKey=APIKEYHERE`)
         .then((resp) => {
             let data = this.state.data.concat(resp.data.articles)
-            this.setState({data})
+            console.log(data)
+            this.setState({
+                data,
+                loading:true
+            })
         })
     }
     render(){
+        const { data } = this.state
         return(
             <div>
-                Will work on this tmrw
+                <Nav className="container">
+                    <Navbar/>
+                </Nav>
+                <div className="container">
+                    <div className="row">
+                        {data && data.map(article => (
+                            <a href={article.url} 
+                            key={article.title} 
+                            className="col-md-4 col-sm-12"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            >
+                                <Thumbnail className="thumbnail">
+                                    <Img src={article.urlToImage} alt={article.title}/>
+                                    <h5>{article.title}</h5>
+                                </Thumbnail>
+                            </a>                                                  
+                        ))}
+                    </div>
+                </div>
             </div>
         )
     }
